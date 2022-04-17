3a7babb05eea67678ae552395ae28f379f9151c4
suryanshm000 <suryanshm000@gmail.com>
2022-04-17 18:45:46 +0530

    Added extra features

--- manage.py
+++ manage.py
@@ -5,8 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flowerEcommerce.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Online_Grocery_Shop.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
